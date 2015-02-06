ef899af55fac2a0512fd4a4ed565ab08780fc15a
Evgeny Zhivitsa <evgeny.zhivitsa@hiqo-solutions.com>
2015-02-06 10:59:22 +0300

    merge

--- develop/js/modules/main.js
+++ develop/js/modules/main.js
@@ -8,53 +8,60 @@ define([
 		'camera',
 		'segment',
 		'hero',
-		// 'obstacle',
 		'animation',
 		'difficulty'
 	],
-	function(THREE, Projector, TrackballControls, Scene, Renderer, Camera, Segment, Hero, /*Obstacle,*/ Animation, Difficulty) {
+	function(THREE, Projector, TrackballControls, Scene, Renderer, Camera, Segment, Hero, Animation, Difficulty) {
 		'use strict';
-
-		var pageSize = {
-				width: window.innerWidth - 10,
-				height: window.innerHeight - 20
-			},
-			heroSize = {
-				radius: 1,
-				widthSegments: 20,
-				heightSegments: 20
-			},
-			segmentSize = {
-				width: 24,
-				height: 24,
-				depth: 12
-			},
-			canvasElement = document.getElementById("WebGLCanvas");
 
 		var diff = new Difficulty();
 
 		var animation = new Animation();
 
-		var renderer = new Renderer(canvasElement, pageSize.width, pageSize.height),
-			camera = new Camera(75, pageSize.width, pageSize.height, 0.1, 1000),
-			//obstacle = new Obstacle( segmentSize.width / 2, segmentSize.depth / 2 ),
-			scene = new Scene(renderer.get(), camera.get(), diff),
-			hero = new Hero(heroSize, diff);
+		THREE.DefaultLoadingManager.onLoad = function (  ) {
 
-		camera.setPosition(0, 0, 11);
+			var pageSize = {
+					width: window.innerWidth - 10,
+					height: window.innerHeight - 20
+				},
+				heroSize = {
+					radius: 1,
+					widthSegments: 20,
+					heightSegments: 20
+				},
+				segmentOptions = {
+					width: 24,
+					height: 24,
+					depth: 12
+				},
+				// obstacleOptions = {
+				// 	width: segmentOptions.width / 2,
+				// 	depth: segmentOptions.depth / 2
+				// },
+				canvasElement = document.getElementById("WebGLCanvas");
 
-		scene.init();
+			var animation = new Animation();
 
-		var NUMOFSEGMENT = 33;
-		for (var i = 0; i < NUMOFSEGMENT; i++) {
-			scene.addSegment( new Segment(segmentSize) );
-		}
-		scene.addHero(hero);
-		scene.render();
-		scene.animate(animation);
-		hero.animate(animation);
-		diff.update(animation);
+			var renderer = new Renderer(canvasElement, pageSize.width, pageSize.height),
+				camera = new Camera(75, pageSize.width, pageSize.height, 0.1, 500),
+				scene = new Scene(renderer.get(), camera.get(), diff),
+				hero = new Hero(heroSize, diff);
 
-		animation.start();
+			camera.setPosition(0, 0, 11);
+
+			scene.init();
+
+			var NUMOFSEGMENT = 33;
+			for (var i = 0; i < NUMOFSEGMENT; i++) {
+				scene.addSegment( new Segment(segmentOptions) );
+			}
+			scene.addHero(hero);
+			scene.render();
+			scene.animate(animation);
+			hero.animate(animation);
+			diff.update(animation);
+
+			animation.start();
+		};
 	}
 );
