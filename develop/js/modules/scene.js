b04a8f85c7fc48ae537907d712d09c2d882b384e
Evgeny Zhivitsa <evgeny.zhivitsa@hiqo-solutions.com>
2015-02-06 10:59:40 +0300

    scene merge

--- develop/js/modules/scene.js
+++ develop/js/modules/scene.js
@@ -27,8 +27,18 @@ define([
 
 			this.ambientLight = new THREE.AmbientLight(0x202020);
 			this.spotLight = new THREE.SpotLight( 0xffffff );
-			this.spotLight.position.set( 0, 10, 60);
+			this.spotLight.position.set( 0, 20, 80);
 			this.spotLight.castShadow = true;
+			// this.spotLight.shadowDarkness = 0.5;
+			// this.spotLight.shadowCameraNear	= 0.01;
+			// this.spotLight.shadowCameraFov	= 45;
+			// this.spotLight.shadowCameraFar	= 250;
+			// this.spotLight.shadowCameraVisible	= false;
+			// this.spotLight.shadowBias	= 0.00;
+			// this.spotLight.shadowDarkness	= 0.6;
+			// this.spotLight.shadowMapWidth = 4096;
+			// this.spotLight.shadowMapHeight = 4096;
+			// this.spotLight.shadowCameraVisible = true;
 		};
 
 		Scene.prototype.render = function () {
@@ -48,30 +58,29 @@ define([
 		Scene.prototype.animate = function (animation) {
 			var self = this;
 
-			animation.addAction(function () {
-				self.updateControls.call(self);
-				self.updateSegments.call(self);
+			animation.addAction(function (delta, now) {
+				self.updateControls.call(self, delta, now);
+				self.updateSegments.call(self, delta, now);
 			});
 		};
 
-		Scene.prototype.updateControls = function () {
+		Scene.prototype.updateControls = function (delta, now) {
 			this.controls.update();
 		};
 
-		Scene.prototype.updateSegments = function () {
+		Scene.prototype.updateSegments = function (delta, now) {
 			for (var i = 0; i < this.segments.length; i++) {
-				this.iteration++;
-
-				(function (segment, pos, speed, diff, self) {
+				(function (segment, pos, speed, diff, self, delta) {
 					if ( Math.floor(pos.z + speed) < (395 - 380) ) {
-						pos.z += speed;
+						pos.z += speed * delta;
 					}
 					else {
-						pos.z += -395 + speed;
+						pos.z += -395 + speed * delta;
 						segment.generateMatrix(diff);
+						console.log(segment.mesh)
 						self.obstacle.refreshSegment( segment.mesh, segment.blockMatrix );
 					}
-				})(this.segments[i], this.segments[i].mesh.position, this.diff.get('speed'), this.diff.get('diff'), this);
+				})(this.segments[i], this.segments[i].mesh.position, this.diff.get('speed'), this.diff.get('diff'), this, delta);
 			}
 
 			this.renderer.render(this.scene, this.camera);
@@ -83,7 +92,7 @@ define([
 
 		Scene.prototype.addSegment = function (segment) {
 			this.segments.push(segment);
-			segment.generateMatrix(this.diff);
+			segment.generateMatrix(this.diff.get('diff'));
 			this.obstacle.addToSegment( segment.mesh, segment.blockMatrix );
 
 			this.scene.add(segment.mesh);
